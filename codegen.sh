9a041c29956c64719b86b5b64b81164f74d3471d
Max Wittal <wittal@neobotix.de>
2020-11-12 18:25:34 +0100

    added build

---
+++ codegen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
+
+VNX_INCLUDE=$1
+
+if [ -z "$VNX_INCLUDE" ]; then
+        VNX_INCLUDE=${VNX_INTERFACE_DIR}/vnx/
+fi
+
+cd $(dirname "$0")
+
+vnxcppcodegen --cleanup generated/ vnx.gtk interface/ modules/ $VNX_INCLUDE
